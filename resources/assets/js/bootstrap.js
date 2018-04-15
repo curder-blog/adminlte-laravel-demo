2f78776c3b0ac413ce7a46ca565c4c31ed143fd6
curder <curder@foxmail.com>
2018-04-15 16:48:46 +0800

    安装acacha/admin-lte-template-laravel拓展包

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,5 +1,6 @@
+/* global _ Vue */
 
-window._ = require('lodash');
+window._ = require('lodash')
 window.Popper = require('popper.js').default;
 
 /**
@@ -9,10 +10,13 @@ window.Popper = require('popper.js').default;
  */
 
 try {
-    window.$ = window.jQuery = require('jquery');
+  window.$ = window.jQuery = require('jquery');
 
-    require('bootstrap');
+  require('bootstrap');
 } catch (e) {}
+
+require('admin-lte')
+require('icheck')
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -20,7 +24,7 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+window.axios = require('axios')
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
@@ -33,10 +37,29 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+  window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+  console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
+
+/**
+ * Vue is a modern JavaScript library for building interactive web interfaces
+ * using reactive data binding and reusable components. Vue's API is clean
+ * and simple, leaving you to focus on building your next great project.
+ */
+
+window.Vue = require('vue')
+
+// Use trans function in Vue (equivalent to trans() Laravel Translations helper). See htmlheader.balde.php partial.
+Vue.prototype.trans = (key) => {
+  return _.get(window.trans, key, key)
+}
+
+// Laravel AdminLTE vue components
+Vue.component('register-form', require('./components/auth/RegisterForm.vue'))
+Vue.component('login-form', require('./components/auth/LoginForm.vue'))
+Vue.component('email-reset-password-form', require('./components/auth/EmailResetPasswordForm.vue'))
+Vue.component('reset-password-form', require('./components/auth/ResetPasswordForm.vue'))
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -50,7 +73,5 @@ if (token) {
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     encrypted: true
+//     key: 'your-pusher-key'
 // });
