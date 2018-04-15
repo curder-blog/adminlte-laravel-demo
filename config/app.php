13a982e929775f0ba76f89387ba96f920cff9972
curder <curder@foxmail.com>
2018-04-15 16:52:07 +0800

    安装spatie/laravel-menu拓展包

--- config/app.php
+++ config/app.php
@@ -208,6 +208,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Html' => Spatie\Menu\Laravel\Html::class,
+        'Link' => Spatie\Menu\Laravel\Link::class,
 
     ],
 
